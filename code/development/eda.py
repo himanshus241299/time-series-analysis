67b01537c72aa9734d81e2e49e6b53d9d278fc8d
himanshu <himanshus241299@gmail.com>
2025-06-03 00:04:53 +0530

    initial project structure

---
+++ code/development/eda.py
@@ -0,0 +1,32 @@
+# This is a placeholder for the main execution of the script
+# You can add your EDA code here
+print("Running EDA script...")
+# Example operation
+print(1 + 1)
+
+
+a = 1
+b = 2
+
+a + b
+a = {
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "f": 6,
+    "g": 7,
+    "h": 8,
+    "i": 9,
+    "j": 10,
+    "k": 11,
+    "l": 12,
+    "m": 13,
+    "n": 14,
+    "o": 15,
+    "p": 16,
+    "q": 17,
+    "r": 18,
+    "s": 19,
+    "t": 20,
+}
