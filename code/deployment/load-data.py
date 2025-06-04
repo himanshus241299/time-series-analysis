af3382d24736bfaa258f167bb7ca424e4f4077dc
himanshu <himanshus241299@gmail.com>
2025-06-05 00:01:06 +0530

    updated button logic in load-data

--- code/deployment/load-data.py
+++ code/deployment/load-data.py
@@ -4,44 +4,105 @@ import utilities as utils
 
 st.header("*Load Data*", divider="gray")
 
-
 st.subheader("*Data Format*")
 
 st.caption("Please Upload Data in following format")
 sample_data = pd.read_excel("data/input/sample_mev_data.xlsx")
 st.dataframe(sample_data, hide_index=True)
 
-uploaded_file = st.file_uploader(
-    label="Upload Data", type=["csv", "xlsx"], label_visibility="hidden"
-)
-if uploaded_file is not None:
-    if uploaded_file.name.endswith(".csv"):
-        df = pd.read_csv(uploaded_file)
-    elif uploaded_file.name.endswith(".xlsx"):
-        df = pd.read_excel(uploaded_file)
+if "upload_data_button" not in st.session_state:
+    st.session_state["upload_data_button"] = False
 
-summary_df = utils.summary(df)
+if "sample_data_button" not in st.session_state:
+    st.session_state["sample_data_button"] = False
 
-st.subheader("*Data Summary*")
+if "button3" not in st.session_state:
+    st.session_state["button3"] = False
+uploaded_file = None
 
-st.data_editor(
-    summary_df,
-    column_config={
-        "Values": st.column_config.LineChartColumn(
-            "Time Series",
-            width="medium",
-            y_min=0,
-            y_max=100,
-        ),
-        "Select": st.column_config.CheckboxColumn(
-            "Select",
-            help="Select your **favorite** widgets",
-            default=True,
-        ),
-    },
-    hide_index=True,
+if st.button("Use Sample Data"):
+    st.session_state["sample_data_button"] = not st.session_state["sample_data_button"]
+    if st.session_state["upload_data_button"]:
+        st.session_state["upload_data_button"] = not st.session_state["upload_data_button"]
 
-)
+if st.button("Upload Data"):
+    st.session_state["upload_data_button"] = not st.session_state["upload_data_button"]
+    if st.session_state["sample_data_button"]:
+        st.session_state["sample_data_button"] = not st.session_state["sample_data_button"]
+    
+if st.session_state["sample_data_button"]:
+    
+    summary_df = utils.summary(sample_data)
 
-if st.button("Exploratory Data Analysis"):
-    st.switch_page("eda.py")
+    st.subheader("*Data Summary*")
+    st.data_editor(
+        summary_df,
+        column_config={
+            "Values": st.column_config.LineChartColumn(
+                "Time Series",
+                width="medium",
+                y_min=0,
+                y_max=100,
+            ),
+            "Select": st.column_config.CheckboxColumn(
+                "Select",
+                help="Select your **favorite** widgets",
+                default=True,
+            ),
+        },
+        hide_index=True,
+    )
+        
+    if st.button("Exploratory Data Analysis"):
+        st.switch_page("eda.py")
+    
+if st.session_state["upload_data_button"]:
+    uploaded_file = st.file_uploader(
+        label="Upload Data", type=["csv", "xlsx"], label_visibility="hidden"
+    )
+
+    if uploaded_file is not None:
+        if uploaded_file.name.endswith(".csv"):
+            df = pd.read_csv(uploaded_file)
+        elif uploaded_file.name.endswith(".xlsx"):
+            df = pd.read_excel(uploaded_file)
+            
+        summary_df = utils.summary(df)
+
+        st.subheader("*Data Summary*")
+        st.data_editor(
+            summary_df,
+            column_config={
+                "Values": st.column_config.LineChartColumn(
+                    "Time Series",
+                    width="medium",
+                    y_min=0,
+                    y_max=100,
+                ),
+                "Select": st.column_config.CheckboxColumn(
+                    "Select",
+                    help="Select your **favorite** widgets",
+                    default=True,
+                ),
+            },
+            hide_index=True,
+        )
+        
+    if st.button("Exploratory Data Analysis"):
+        st.switch_page("eda.py")
+    
+    
+
+
+
+# Print the session state to make it easier to see what's happening
+# st.write(
+#     f"""
+#     ## Session state:
+#     {st.session_state["upload_data_button"]=}
+
+#     {st.session_state["sample_data_button"]=}
+
+#     {st.session_state["button3"]=}
+#     """
+# )
